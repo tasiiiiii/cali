5193854472ff63dcb903d3d4ddb42e7a9c27e0f2
admin <442953895@qq.com>
2024-02-22 14:02:22 +0800

    修改代码

--- db/queries/guestbook.ts
+++ db/queries/guestbook.ts
@@ -8,7 +8,9 @@ export async function fetchGuestbookMessages({
   limit = 200,
 }: { limit?: number } = {}) {
   console.log(guestbook)
-  const data = await db
+  let data = []
+  try{
+     data = await db
     .select({
       id: guestbook.id,
       userId: guestbook.userId,
@@ -19,15 +21,18 @@ export async function fetchGuestbookMessages({
     .from(guestbook)
     .orderBy(desc(guestbook.createdAt))
     .limit(limit)
-  // const data = [{id:12,
-  //   userId: '122',
-  //   userInfo: {
-  //     firstName: '2326',
-  //     lastName: '88989',
-  //     imageUrl: null
-  //   },
-  //   message: '6666666',
-  //   createdAt: '2'}]
+  }catch{
+     data = [{id:12,
+      userId: '122',
+      userInfo: {
+        firstName: '2326',
+        lastName: '88989',
+        imageUrl: null
+      },
+      message: '6666666',
+      createdAt: '2'}]
+  }
+  
   return data.map(
     ({ id, ...rest }) =>
       ({
