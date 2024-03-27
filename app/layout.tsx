601e977cb3530979992a24ab6e54efe951c04790
admin <442953895@qq.com>
2024-03-27 17:20:40 +0800

    修改容错判断为 null

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
     siteName: 'Cali Castle',
     locale: 'zh_CN',
     type: 'website',
-    url: 'https://cali.so',
+    url: 'https://cali-ochre.vercel.app',
   },
   twitter: {
     site: '@thecalicastle',
